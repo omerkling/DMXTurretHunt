31b2f2bb4bb822b3d5150c9500cc6bd49a2ed31c
Owen Merkling <omerkling@users.noreply.github.com>
2020-09-13 15:15:50 -0600

    Initial commit

---
+++ Source/TestDMX/TestDMXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestDMXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTDMX_API ATestDMXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
